089df544c9c08c2fbd332d69ed8bbbf7a4e4c5df
Nathi72TcF <tcfworld72@gmail.com>
2022-04-05 14:49:47 +0200

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'kasihookupapp',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
